61a34044f97e8ba4ef50730bf1249c1d29c1fcd8
tbranyen <tim@tabdeveloper.com>
2012-08-28 23:12:25 -0400

    starting over with backbone-boilerplate and started porting over lauren's code into the new system

---
+++ app/router.js
@@ -0,0 +1,52 @@
+define([
+  // Application.
+  "app"
+],
+
+function(app) {
+
+  // Defining the application router, you can attach sub routers here.
+  var Router = Backbone.Router.extend({
+    routes: {
+      // Get access to arguments.
+      ":args": "index"
+    },
+
+    index: function() {
+      // Send up options.
+      app.socket.send(JSON.stringify({
+        loadDoc: {
+          // Pass up the document name if it's set.
+          docName: this.qs.docName,
+
+          // TODO What is this?
+          delay: parseFloat(this.qs.delay, 10)
+        }
+      }));
+    },
+
+    initialize: function() {
+      // Cache the querystring lookup.
+      var querystring = location.search.slice(1);
+
+      // For every key/value pair, break into [key] = value onto the `qs`
+      // router property.
+      Object.defineProperty(this, "qs", {
+        // Whenever the property is accessed process the latest value.
+        get: function() {
+          return querystring.split("&").reduce(function(memo, keyVal) {
+            // Break the keyVal string into actual key/value pairs.
+            var parts = keyVal.split("=");
+            // Assign them into the memoized object, which will be `this.qs`.
+            memo[parts[0]] = parts[1];
+
+            return memo;
+          }, {});
+        }
+      });
+    }
+  });
+
+  return Router;
+
+});

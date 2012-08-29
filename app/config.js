bd170aacb5bd06855856950a1671e1cd8888ef0f
tbranyen <tim@tabdeveloper.com>
2012-08-29 00:21:40 -0400

    changed to app/core, created a baseConfig

--- app/config.js
+++ app/config.js
@@ -1,29 +1,7 @@
 // Set the require.js configuration for your application.
 require.config({
 
-  // Initialize the application with the main application file.
-  deps: ["core/main"],
-
-  paths: {
-    // JavaScript folders.
-    libs: "../vendor/js/libs",
-    plugins: "../vendor/js/plugins",
-    vendor: "../vendor/vendor",
-    core: "../core",
-
-    // Libraries.
-    jquery: "../vendor/js/libs/jquery",
-    lodash: "../vendor/js/libs/lodash",
-    backbone: "../vendor/js/libs/backbone"
-  },
-
   shim: {
-    // Backbone library depends on lodash and jQuery.
-    backbone: {
-      deps: ["lodash", "jquery"],
-      exports: "Backbone"
-    },
-
     "libs/engine.io": {
       exports: "eio"
     },

787dcb6d395ff6990fa89a14196335b59e15e455
Lauren McCarthy <laurmccarthy@gmail.com>
2012-10-02 15:54:18 -0400

    fixing socket connection

--- app/app.js
+++ app/app.js
@@ -22,8 +22,10 @@ function($, _, Backbone, eio) {
     views: {},
 
     // Create a socket connection to the server.
-    //socket: new eio.Socket({ host: location.hostname, port: 8081 })
-    socket: new eio.Socket({ host: "198.61.191.97", port: 8081 })
+    
+    
+    socket: new eio.Socket({ host: location.hostname, port: 8081 })
+    //socket: new eio.Socket({ host: "198.61.191.97", port: 8081 })
     
   };
 
